3f37cda24b9ef55ce78ac7523136334d4cd24901
Lukas Wilde <s8luwild@stud.uni-saarland.de>
2022-12-06 08:30:38 +0100

    Added day06

---
+++ day06/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+use utils::parse_text;
+
+fn main() {
+    let text = parse_text();
+    let packet_pos = find_packet_marker_pos(&text, 4);
+    let message_pos = find_packet_marker_pos(&text, 14);
+    println!("The first position after a packet marker is {}", packet_pos);
+    println!(
+        "The first position after a message marker is {}",
+        message_pos
+    );
+}
+
+fn is_unique(sequence: impl Iterator<Item = char>) -> bool {
+    let mut already_seen: HashSet<char> = HashSet::new();
+    for c in sequence {
+        if already_seen.contains(&c) {
+            return false;
+        }
+        already_seen.insert(c);
+    }
+    true
+}
+
+fn find_packet_marker_pos(text: &str, num_distinct: usize) -> usize {
+    text.chars()
+        .collect::<Vec<char>>()
+        .windows(num_distinct)
+        .enumerate()
+        .find(|(_, chunk)| is_unique(chunk.iter().cloned()))
+        .expect("There should be a start marker")
+        .0
+        + num_distinct
+}
---
+++ day06/tests/communication.rs
@@ -0,0 +1,79 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn first() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("day06")?;
+    cmd.arg("bvwbjplbgvbhsrlpgdmjqwftvncz");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The first position after a packet marker is 5",
+        ))
+        .stdout(predicate::str::contains(
+            "The first position after a message marker is 23",
+        ));
+
+    Ok(())
+}
+
+#[test]
+fn second() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("day06")?;
+    cmd.arg("nppdvjthqldpwncqszvftbrmjlhg");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The first position after a packet marker is 6",
+        ))
+        .stdout(predicate::str::contains(
+            "The first position after a message marker is 23",
+        ));
+    Ok(())
+}
+
+#[test]
+fn third() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("day06")?;
+    cmd.arg("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The first position after a packet marker is 10",
+        ))
+        .stdout(predicate::str::contains(
+            "The first position after a message marker is 29",
+        ));
+    Ok(())
+}
+
+#[test]
+fn fourth() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("day06")?;
+    cmd.arg("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The first position after a packet marker is 11",
+        ))
+        .stdout(predicate::str::contains(
+            "The first position after a message marker is 26",
+        ));
+    Ok(())
+}
+
+#[test]
+fn fifth() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("day06")?;
+    cmd.arg("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains(
+            "The first position after a packet marker is 7",
+        ))
+        .stdout(predicate::str::contains(
+            "The first position after a message marker is 19",
+        ));
+    Ok(())
+}
